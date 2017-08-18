31fd96ea0f2c4805360532aa52d6cb73c2ef0265
Edward Clinton <edward.t.clinton@gmail.com>
2017-08-18 18:49:06 -0500

    store orders in db

--- src/pages/received-orders/received-orders.ts
+++ src/pages/received-orders/received-orders.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AlertController } from 'ionic-angular';
+import { ItemSliding } from 'ionic-angular';
 
 // import { AddProducePage } from '../add-produce/add-produce';
 
@@ -8,5 +11,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'received-orders.html'
 })
 export class ReceivedOrders {
-  constructor(public navCtrl: NavController) {}
+  private orders: FirebaseListObservable<any[]>;
+  constructor(public navCtrl: NavController, public af_db: AngularFireDatabase, public alertCtrl: AlertController) {
+    this.orders = af_db.list('/orders');
+
+
+  }
+   public removeItem(slidingItem: ItemSliding, item: any) {
+
+    this.orders.remove(item.$key);
+
+    slidingItem.close();
+
+  }
 }
