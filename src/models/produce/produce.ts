8585c7ed470d26d759a227b31444fa0900c622ee
Edward Clinton <edward.t.clinton@gmail.com>
2017-12-03 17:44:15 -0600

    added ability to mark an order as done

--- src/models/produce/produce.ts
+++ src/models/produce/produce.ts
@@ -26,4 +26,5 @@ export interface Produce {
   orderByUser : string;
   orderByFB : string;
   orderByDomain: string;
+  orderDone: boolean;
 }
--- src/pages/received-orders/received-orders.ts
+++ src/pages/received-orders/received-orders.ts
@@ -3,7 +3,7 @@ import { NavController } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AlertController } from 'ionic-angular';
 import { ItemSliding } from 'ionic-angular';
-import {AuthenticationService} from '../../services/AuthenticationService';
+import { AuthenticationService } from '../../services/AuthenticationService';
 
 // import { AddProducePage } from '../add-produce/add-produce';
 
@@ -17,30 +17,32 @@ export class ReceivedOrders {
     this.orders = af_db.list('/orders');
     console.log("test")
     console.log(this.authService.getDomain())
-    if(this.authService.getDomain() != "@feedingtexas.org")
-      {
-        this.orders = this.af_db.list('/orders',
+    if (this.authService.getDomain() != "@feedingtexas.org") {
+      this.orders = this.af_db.list('/orders',
         {
           query: {
             orderByChild: 'orderByDomain',
-            equalTo : this.authService.getDomain()
+            equalTo: this.authService.getDomain()
           }
         }
       );
 
-      }
-  
+    }
+
 
 
   }
-   public removeItem(slidingItem: ItemSliding, item: any) {
+  public removeItem(slidingItem: ItemSliding, item: any) {
 
     this.orders.remove(item.$key);
 
-    if(slidingItem)
-    {
+    if (slidingItem) {
       slidingItem.close();
-      }
+    }
 
+  }
+
+  public updateStatus(slidingItem: ItemSliding, item:any){
+    
   }
 }
